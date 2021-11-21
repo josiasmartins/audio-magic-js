d92142bc41ef7258a34464c2d79254d5fdb71c4c
Caitano <josiasmartinscaitano@gmail.com>
2021-11-21 14:51:58 -0300

    começando a musica com js

---
+++ visualizer.js
@@ -0,0 +1,6 @@
+function main() {
+    const canvas = document.getElementById('myCanvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
